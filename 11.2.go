90b828dda24b6e1f6bd433902f14108881012225
刘2022 <109359021+liukk222@users.noreply.github.com>
2022-11-03 13:58:28 +0800

    Add files via upload

--- 11.2.go
+++ 11.2.go
@@ -2,6 +2,31 @@ package main
 
 import "fmt"
 
+func getNamendAge() (string, int) {
+	return "tom", 20
+}
+
 func main() {
-	fmt.Print("hello go")
+	// var (
+	// name string="tom"
+	// age  int=12
+	// b    bool=true
+	// )
+	// fmt.Printf("name: %v\n", name)
+	// fmt.Printf("age: %v\n", age)
+	// fmt.Printf("b: %v\n", b)
+	// var name="tom"
+	// var age =20
+	// var b=true
+	// fmt.Printf("name: %v\n", name)
+	// fmt.Printf("age: %v\n", age)
+	// fmt.Printf("b: %v\n", b)
+	// name :="tom"
+	// age:=20
+	name, age := getNamendAge()
+	b := true
+	fmt.Printf("name: %v\n", name)
+	fmt.Printf("age: %v\n", age)
+	fmt.Printf("b: %v\n", b)
+	// asdsffsd
 }
