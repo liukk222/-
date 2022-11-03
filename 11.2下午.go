f105f6e474ae3e7ad7738a5b01c72cf42100965c
刘2022 <109359021+liukk222@users.noreply.github.com>
2022-11-03 14:34:39 +0800

    Add files via upload

---
+++ 11.2下午.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+//import "fmt"
+
+func main() {
+	/* const PI float32 = 3.14 */
+	/* const PI2 = 3.1415 */
+	/* fmt.Printf("PI: %v\n", PI) */
+	/* fmt.Printf("PI2: %v\n", PI2) */
+	/* const( */
+	/* 	a=100 */
+	/* 	b=200 */
+	/* ) */
+	/* fmt.Printf("a: %v\n", a) */
+	/* fmt.Printf("b: %v\n", b) */
+	/* const w,h=200,300 */
+	/* fmt.Printf("w: %v\n", w) */
+	/* fmt.Printf("h: %v\n", h) */
+	/* const( */
+	/* 	a=iota */
+	/* 	b=iota */
+	/* 	c=iota */
+	/* ) */
+	/* fmt.Printf("a: %v\n", a) */
+	/* fmt.Printf("b: %v\n", b) */
+	/* fmt.Printf("c: %v\n", c) */
+	/* const( */
+	/* 	a1=iota */
+	/* 	a=100 */
+	/* 	a2=iota */
+	/*  */
+	/* ) */
+	/* fmt.Printf("a1: %v\n", a1) */
+	/* fmt.Printf("a: %v\n", a) */
+	/* fmt.Printf("a2: %v\n", a2) */
+	/*  */
+	/* var name string = "tom" */
+	/* age := 20 */
+	/* b := true */
+	/* fmt.Printf("name: %v\n", name) */
+	/* fmt.Printf("%d\n", age) */
+	/* fmt.Printf("%T\n", b) */
+	/* a := 100 */
+	/* p := &a */
+	/* fmt.Printf("p: %T\n", p) */
+	a := [2]int{1, 2}
+	fmt.Printf("%d\n", a[0])
+}
