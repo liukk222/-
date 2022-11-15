07e360ac63218692136c1567ec136b64cd07335b
刘2022 <109359021+liukk222@users.noreply.github.com>
2022-11-15 22:03:50 +0800

    Add files via upload

---
+++ 11.15.3.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var wp sync.WaitGroup
+
+func showMsg(i int) {
+	defer wp.Add(-1)
+	fmt.Printf("i: %v\n", i)
+}
+
+func main() {
+	for i := 0; i < 10; i++ {
+		go showMsg(i)
+		wp.Add(1)
+	}
+	wp.Wait()
+	fmt.Println("end...")
+}
