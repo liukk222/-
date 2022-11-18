80766e25c9b8d2053caf527bb419d485b3e23339
刘2022 <109359021+liukk222@users.noreply.github.com>
2022-11-19 00:48:08 +0800

    Add files via upload

---
+++ 11.18.3.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"sync/atomic"
+)
+
+func test_add_sub() {
+	var i int32 = 100
+	atomic.AddInt32(&i, 1)
+	fmt.Printf("i: %v\n", i)
+	atomic.AddInt32(&i, -1)
+	fmt.Printf("i: %v\n", i)
+	var j int64 = 200
+	atomic.AddInt64(&j, 1)
+	fmt.Printf("j: %v\n", j)
+}
+
+func tset_load_store() {
+	var i int32 = 100
+	atomic.LoadInt32(&i)
+	fmt.Printf("i: %v\n", i)
+	atomic.StoreInt32(&i, 200)
+	fmt.Printf("i: %v\n", i)
+}
+
+func main() {
+	//tset_load_store()
+	var i int32 = 100
+	b := atomic.CompareAndSwapInt32(&i, 100, 200)
+	fmt.Printf("b: %v\n", b)
+	fmt.Printf("i: %v\n", i)
+
+}
