9f71ceca3687ba201edfc338e32c96b2277a7c6a
刘2022 <109359021+liukk222@users.noreply.github.com>
2022-12-01 19:47:01 +0800

    Add files via upload

---
+++ 12.1.3.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var client *mongo.Client
+
+func initDb() {
+	co := options.Client().ApplyURI("mongodb://localhost:27017")
+	c, err := mongo.Connect(context.TODO(), co)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err2 := c.Ping(context.TODO(), nil)
+	if err2 != nil {
+		log.Fatal(err2)
+	} else {
+		fmt.Println("连接成功！")
+	}
+}
+
+func main() {
+	initDb()
+}
