3e5a9e86cf175b946a2580831e4e272ff03bded9
刘2022 <109359021+liukk222@users.noreply.github.com>
2022-12-01 23:28:21 +0800

    Add files via upload

---
+++ 12.1.5.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var client *mongo.Client
+
+func initDb() {
+	co := options.Client().ApplyURI("mongodb://localhost:27017")
+	c, err := mongo.Connect(context.TODO(), co)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err2 := c.Ping(context.TODO(), nil)
+	if err2 != nil {
+		log.Fatal(err2)
+	} else {
+		fmt.Println("连接成功！")
+	}
+	client = c
+}
+
+func find() {
+	ctx := context.TODO()
+	//defer client.Disconnect(ctx)
+	//defer client.Disconnect(context.TODO())
+	c := client.Database("go_db").Collection("Student")
+	c2, err := c.Find(ctx, bson.D{{"name", "tom"}})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer c2.Close(ctx)
+	defer client.Disconnect(ctx)
+	for c2.Next(ctx) {
+		var result bson.D
+		c2.Decode(&result)
+		fmt.Printf("result: %v\n", result)
+		fmt.Printf("result.Map(): %v\n", result.Map())
+	}
+}
+
+func main() {
+	initDb()
+	find()
+}
