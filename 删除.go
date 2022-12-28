5d90b8c6d60e7b92c32320c244065ed3d4ea0faa
刘2022 <109359021+liukk222@users.noreply.github.com>
2022-12-28 15:46:49 +0800

    Add files via upload

---
+++ 删除.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+func init() {
+	dsn := "root:2003925liu@tcp(127.0.0.1:3306)/golang_db?charset=utf8mb4&parseTime=True"
+	d, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	db = d
+}
+
+type User struct {
+	gorm.Model
+	Name     string
+	Age      int
+	Birthday time.Time
+	Active   bool
+}
+
+func createTable() {
+	db.AutoMigrate(&User{})
+}
+func insert() {
+	user := User{
+		Name:     "tom",
+		Age:      20,
+		Birthday: time.Now(),
+		Active:   true,
+	}
+	db.Create(&user)
+}
+
+func del1() {
+	var uesr User
+	db.First(&uesr)
+	db.Delete(&uesr)
+}
+func del2() {
+	db.Delete(&User{}, 2)
+}
+func (u *User) BeforeDelete(tx *gorm.DB) (err error) {
+	fmt.Println("delete")
+	return nil
+}
+func main() {
+	del2()
+}
