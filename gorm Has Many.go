1a350e1b4ff77056de6874ff76b97d4ffa75be92
刘2022 <109359021+liukk222@users.noreply.github.com>
2022-12-29 20:52:49 +0800

    Add files via upload

---
+++ gorm Has Many.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+func init() {
+	dsn := "root:2003925liu@tcp(127.0.0.1:3306)/golang_db?charset=utf8mb4&parseTime=True"
+	d, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	db = d
+}
+
+type User struct {
+	gorm.Model
+	CreditCards []CreditCards
+}
+type CreditCards struct {
+	gorm.Model
+	Number string
+	UserID uint
+}
+
+func createTable() {
+	db.AutoMigrate(&User{}, &CreditCards{})
+
+}
+
+func main() {
+	createTable()
+}
