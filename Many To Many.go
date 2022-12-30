ceaecbe4eb354374fa517c6f35ec5c5e6a3e3eb2
刘2022 <109359021+liukk222@users.noreply.github.com>
2022-12-30 16:47:23 +0800

    Add files via upload

---
+++ Many To Many.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+func init() {
+	dsn := "root:2003925liu@tcp(127.0.0.1:3306)/golang_db?charset=utf8mb4&parseTime=True"
+	d, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	db = d
+}
+
+// User 拥有并属于多种 language，`user_languages` 是连接表
+type User struct {
+	gorm.Model
+	Languages []Language `gorm:"many2many:user_languages;"`
+}
+
+type Language struct {
+	gorm.Model
+	Name string
+}
+
+func createTable() {
+	db.AutoMigrate(&User{}, &Language{})
+}
+func insert() {
+
+	l := Language{
+		Name: "english",
+	}
+
+	user := User{
+		Languages: []Language{l},
+	}
+	db.Create(&user)
+	db.Create(&l)
+}
+func main() {
+	insert()
+	//createTable()
+}
